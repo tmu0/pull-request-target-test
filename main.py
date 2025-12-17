6a610b67fa8c9cdd11cf3ba34c45ed6b94f87704
Thomas Müller <thomas.mueller@dfinity.org>
2025-12-17 19:05:20 +0100

    edit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ GITHUB_PR_SHA = os.environ.get('GITHUB_PR_SHA','not set')
 def main():
     print(f"MERGE_COMMIT_SHA = {MERGE_COMMIT_SHA}")
     print(f"GITHUB_PR_SHA = {GITHUB_PR_SHA}")
-    print("internal pr 2")
+    print("internal pr 3")
     if GITHUB_PR_SHA != 'not set':
         with open(os.path.join(GITHUB_PR_SHA, 'README.md'), 'r') as readme:
             print(readme.read())
